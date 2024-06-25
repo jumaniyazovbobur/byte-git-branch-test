4b5b74d1967f39369bc270c43549a410965cd91b
Sardorlion1986 <sardordoridarmon@gmail.com>
2024-06-25 21:35:55 +0500

    save and test2

--- src/Student.java
+++ src/Student.java
@@ -3,4 +3,9 @@ public class Student {
     public void test(){
         System.out.println("Student test");
     }
+
+    public static void save(String[] args) {
+        System.out.println("Student main");
+    }
+    public void test2(){}
 }
