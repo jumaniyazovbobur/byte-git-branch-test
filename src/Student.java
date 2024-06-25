700f306230c2e249acc647e84246c38990e1b892
Sardorlion1986 <sardordoridarmon@gmail.com>
2024-06-25 21:11:56 +0500

    read and test2

--- src/Student.java
+++ src/Student.java
@@ -3,4 +3,12 @@ public class Student {
     public void test(){
         System.out.println("Student test");
     }
+
+    public  void test2(){
+        System.out.println("read");
+    }
+
+    public  void read(){
+        System.out.println("read");
+    }
 }
