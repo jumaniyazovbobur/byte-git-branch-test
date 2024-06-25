af1f09862fd53ce2eccd9aa56bc4ae1ed86b8309
urinovshokirjon <urinovshokirjon93@gmail.com>
2024-06-25 21:34:21 +0500

    Book title

--- src/Book.java
+++ src/Book.java
@@ -3,4 +3,7 @@ public class Book {
     private String title;
     private String author;
     public Book(String title, String author) {}
+    public String getTitle() {
+        return title;
+    }
 }
