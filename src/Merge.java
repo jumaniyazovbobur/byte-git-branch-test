0a47d3a279e80313675fa21f72b515dbf9433076
jumaniyazovbobur <jumaniyazov.bobur@mail.ru>
2024-06-27 20:13:39 +0300

    merge-test 1

--- src/Merge.java
+++ src/Merge.java
@@ -3,5 +3,10 @@ public class Merge {
     public static void test(){
         System.out.println("");
         System.out.println("TEst");
+        System.out.println("TEst");
+        System.out.println("TEst");
+        System.out.println("TEst");
+        System.out.println("TEst");
+        System.out.println("TEst");
     }
 }
