56a97554d0b23726247245f680707df274d20413
jumaniyazovbobur <jumaniyazov.bobur@mail.ru>
2024-06-27 20:23:49 +0300

    master change

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("efwfewfwfweefw");
         System.out.println("efwfewfwfweefw");
+        System.out.println("efwfewfwfweefw");
+        System.out.println("efwfewfwfweefw");
     }
 }
