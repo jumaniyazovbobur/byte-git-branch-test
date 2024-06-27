f540b8bafabc883eead5594e9ac3e7f721b07520
iProgrammeruz <couragesland77@gmail.com>
2024-06-27 21:22:16 +0500

    some changes added

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        System.out.println("Hello world!"); //ok
         System.out.println("efwfewfwfweefw");
         System.out.println("efwfewfwfweefw");
         System.out.println("efwfewfwfweefw");
         System.out.println("efwfewfwfweefw");
     }
+
 }
