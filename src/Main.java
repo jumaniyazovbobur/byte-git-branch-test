03e6a11460115b84f12e8e503220960bb57a0f24
jumaniyazovbobur <jumaniyazov.bobur@mail.ru>
2024-06-27 20:23:28 +0300

    master-2 1

--- src/Main.java
+++ src/Main.java
@@ -4,5 +4,6 @@ public class Main {
         System.out.println("efwfewfwfweefw");
         System.out.println("efwfewfwfweefw");
         System.out.println("efwfewfwfweefw");
+        System.out.println("efwfewfwfweefw");
     }
 }
