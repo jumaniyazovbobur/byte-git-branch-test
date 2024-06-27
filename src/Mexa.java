bf48ac0b064112f3458a612ee04395ce78007fdb
DizzzY0 <20041017i@gmail.com>
2024-06-27 19:34:02 +0500

    Update Class Mexa

--- src/Mexa.java
+++ src/Mexa.java
@@ -1,9 +1,17 @@
+import java.nio.file.attribute.UserPrincipal;
+
 public class Mexa {
     private static int letOtRodu;
     private static String from;
-
+    private static UserPrincipal userPrincipal;
     public static void main(String[] args) {
         from = "Uzbekistan";
         System.out.println(from);
+        userPrincipal = new UserPrincipal() {
+            @Override
+            public String getName() {
+                return "Zero";
+            }
+        };
     }
 }
