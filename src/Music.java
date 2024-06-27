4936e36d5fbfa3f14f5a87a0dec4c57738360951
Doniy07 <93323636+Doniy07@users.noreply.github.com>
2024-06-27 19:26:52 +0500

    Music added

---
+++ src/Music.java
@@ -0,0 +1,10 @@
+public class Music {
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+    // fgsdfsdfdsf
+
+    public static void music() {
+        System.out.println("Hello World!");
+    }
+}
