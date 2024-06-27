be6862689fea9878f63ce81e0c92d4f55f408763
DizzzY0 <20041017i@gmail.com>
2024-06-27 19:28:24 +0500

    Create Class Mexa

---
+++ src/Mexa.java
@@ -0,0 +1,9 @@
+public class Mexa {
+    private static int letOtRodu;
+    private static String from;
+
+    public static void main(String[] args) {
+        from = "Uzbekistan";
+        System.out.println(from);
+    }
+}
